6a156df3ba683134ac11e6bb61f714e17aba85e8
Max V Grushin <support@ids.spb.ru>
2024-02-14 18:05:26 +0300

    импортирован слайдер

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,3 +1,7 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "./common/init";
 
 import "./partials/header";
---
+++ src/assets/scripts/page/home.js
@@ -0,0 +1,67 @@
+import Swiper from "swiper";
+// import PanelSnap from "../../libs/panelsnap/panelsnap";
+// import "particles.js";
+
+import {
+  Navigation,
+  Pagination,
+  EffectFade,
+  Autoplay,
+  Thumbs,
+  Mousewheel,
+} from "swiper/modules";
+// import { scrollDirection } from "../../global/var";
+// import { MediaQuery } from "../../global/func";
+// import { breakpoint } from "../../global/settings";
+// import { Collapse } from "../../utils/collapse";
+
+export const hero = document.querySelector("#home-hero");
+
+// const heroVideos = document.querySelectorAll("#home-hero video");
+
+new Swiper("#home-hero .swiper", {
+  modules: [Navigation, Pagination, EffectFade, Autoplay],
+
+  slidesPerView: 2,
+  spaceBetween: 0,
+  loop: true,
+
+  // speed: 2000,
+  // simulateTouch: false,
+
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false,
+  // },
+
+  // effect: "fade",
+  // fadeEffect: {
+  //   crossFade: true,
+  // },
+
+  navigation: {
+    nextEl: "#home-hero .swiper .swiper-button-next",
+    prevEl: "#home-hero .swiper .swiper-button-prev",
+  },
+
+  pagination: {
+    el: "#home-hero .swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+
+  // on: {
+  //   slideChangeTransitionStart: function (instance) {
+  //     for (const video of heroVideos) {
+  //       video.pause();
+  //     }
+
+  //     const activeIndex = instance.activeIndex;
+  //     const activeSlide = instance.slides[activeIndex];
+  //     const activeSlideVideo = activeSlide.querySelectorAll("video")[0];
+  //     if (activeSlideVideo) {
+  //       activeSlideVideo.play();
+  //     }
+  //   },
+  // },
+});
