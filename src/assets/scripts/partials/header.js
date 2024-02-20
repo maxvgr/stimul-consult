a9ef06d8e795c07ad0274adff5c24429b314cd8f
Max V Grushin <support@ids.spb.ru>
2024-02-20 13:45:41 +0300

    js для плавной прокрутки

--- src/assets/scripts/partials/header.js
+++ src/assets/scripts/partials/header.js
@@ -2,3 +2,23 @@ import { SetSize } from "../base/functions";
 
 const header = document.getElementById("header");
 const headerHeight = SetSize(header, "header", true);
+
+
+
+
+// плавная прокрутка до id
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+
